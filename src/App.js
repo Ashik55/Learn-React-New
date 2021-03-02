e77c23cea33ebea693f7bd071a6ca8760c33717f
aashiq <ashik.cse.developer@gmail.com>
2021-03-03 00:36:36 +0600

    chnage name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import {useState} from "react";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-      <div>
-        <h1>Changes from Rakhi-- updated</h1>
-      </div>
-  );
+    const [name, setName] = useState("ashik")
+
+    function abc() {
+        console.log("Hello")
+        console.log(name)
+    }
+
+    function changeName(){
+        setName("Rakib")
+    }
+
+
+    return (
+        <div>
+            <Button onClick={abc}>Hello</Button>
+            <br/>             <br/>             <br/>
+            <Button onClick={changeName}>Button 2</Button>
+            <br/>             <br/>             <br/>
+
+            <p>{name}</p>
+
+        </div>
+    );
 }
 
 export default App;
